2382af38874cb98ed81decd6f2c9eb0bf7a7291e
Cristobal <71562806+acidghoulfrost@users.noreply.github.com>
2024-05-14 00:13:36 -0400

    Encargo 2

--- Js/validacion.js
+++ Js/validacion.js
@@ -1,1 +1,13 @@
 var tipoopinion = document.getElementById('tipoopinion');
+var textarea = document.getElementById("areatexto");
+var error = document.getElementById("error")
+error.style.color = 'red';
+
+function enviarFormulario(){
+    console.log('Enviando formulario..');
+
+    var MensajesError = [];
+
+    if(textarea.value === null || tipoopinion === ''){}
+        MensajesError.push('El texto no debe estar vacio!')
+}
